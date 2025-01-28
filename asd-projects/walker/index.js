8f709bb8d2d9717d28c9cfc1e621a61a9d22732b
Jamiron15 Varnado <jyrenvarnado@gmail.com>
2025-01-28 13:45:56 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    LEFT: 65,
+    RIGHT: 68,
+    UP: 87,
+    DOWN: 83,
+  }
+  var walker = {
+   positionX: 0,
+   speedX: 0,
+   positionY: 0,
+   speedY: 0,
+  }
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,15 +38,27 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem()
+    redrawGameItem()
 
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+  if (event.which === KEY.LEFT){
+    walker.speedX = -5;
+  }
+  if (event.which === KEY.RIGHT) {
+    walker.speedX = 5;
+  }
+  if (event.which === KEY.UP){
+    walker.speedY = -5;
+  }
+  if (event.which === KEY.DOWN){
+    walker.speedY = 5;
+  }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -52,3 +75,16 @@ function runProgram(){
   }
   
 }
+
+function repositionGameItem(){
+walker.positionX += walker.speedX;
+walker.positionY += walker.speedY; 
+}
+
+function redrawGameItem(){
+  $("#walker").css("left", walker.positionX);
+  $("#walker").css("right", walker.positionX);
+  $("#walker").css("down", walker.positionY);
+  $("#walker").css("top", walker.positionY);
+}
+
