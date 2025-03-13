d46e7a661ca4ce635a8de1b82a5c7dc2257d8080
Jamiron15 Varnado <jyrenvarnado@gmail.com>
2025-03-13 13:16:28 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+applyFilter(reddify)
   
 
   // do not change the below line of code
@@ -32,18 +32,53 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction){
+for(var i = 0; i < image.length; i++){
+var row = image[i]
+for(var x = 0; x < row.length; x++){
+  var rgbString = row[x]
+  var rgbNumbers = rgbStringToArray(rgbString)
+  filterFunction(rgbNumbers)
+  rgbString = rgbArrayToString(rgbNumbers)
+  row[x] = rgbString
+}
+}
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction){
+  var noName = image[0][0];
+  for(var i = 0; g < image.length; g++){
+    var row = image[i];
+    for(var e = 0; e < row.length; e++){
+      var rgbString = row[e];
+      if (rgbString !== background){
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers);
+        row[e] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(number){
+var temp1 = Math.min(255, number);
+var temp2 = Math.max(0, temp1);
+return temp2;
+}
 
 // TODO 3: Create reddify function
-
+function reddify(rgbArray){
+  rgbArray[RED] = 200
+}
 
 // TODO 6: Create more filter functions
-
-
+function decreaseBlue(arr){
+  rgbArray[BLUE] = keepInBounds(rbaArray[BLUE] - 50);
+}
+ function increaseGreenByBlue(filterFunction){
+  rgbArray[GREEN] = keepInBounds(rgbArray[GREEN] + rgbArray[BLUE])
+ }
 // CHALLENGE code goes below here
--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,6 +1,7 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
+
   
 function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
@@ -16,6 +17,14 @@ function runProgram(){
     UP: 87,
     DOWN: 83,
   }
+
+  var Key = {
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68,
+  }
+
   var walker = {
    positionX: 0,
    speedX: 0,
@@ -27,7 +36,9 @@ function runProgram(){
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp);
+  // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -38,11 +49,10 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    repositionGameItem()
-    redrawGameItem()
-
+    repositionGameItem();
+    redrawGameItem();
+    wallCollision();
   }
-  
   /* 
   Called in response to events.
   */
@@ -60,23 +70,41 @@ function runProgram(){
     walker.speedY = 5;
   }
   }
-
+function handleKeyUp(){
+  if (event.which === KEY.LEFT){
+    walker.speedX = 0;
+  }
+  if (event.which === KEY.RIGHT) {
+    walker.speedX = 0;
+  }
+  if (event.which === KEY.UP){
+    walker.speedY = 0;
+  }
+  if (event.which === KEY.DOWN){
+    walker.speedY = 0;
+  }
+}
+var height = $("#board").height(wallCollision)
+var width = $("#board").width(wallCollision)
+function wallCollision(){
+  if (walker.positionX > 385){
+    walker.positionX = 385
+  }
+  if (walker.positionY > 385){
+    walker.positionY = 385
+  }
+  if (walker.positionX < 0){
+    walker.positionY = 0
+  }
+  if (walker.positionY < 0){
+    walker.positionY = 0
+  }
+}
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
-  function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
-
-    // turn off event handlers
-    $(document).off();
-  }
-  
-}
-
-function repositionGameItem(){
+  function repositionGameItem(){
 walker.positionX += walker.speedX;
 walker.positionY += walker.speedY; 
 }
@@ -86,5 +114,14 @@ function redrawGameItem(){
   $("#walker").css("right", walker.positionX);
   $("#walker").css("down", walker.positionY);
   $("#walker").css("top", walker.positionY);
+}
+  function endGame() {
+    // stop the interval timer
+    clearInterval(interval);
+
+    // turn off event handlers
+    $(document).off();
+  }
+  
 }
 
